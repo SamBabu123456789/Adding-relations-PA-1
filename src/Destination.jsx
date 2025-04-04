fc4357fe7793c171b2db0803def29bce84ec6d4d
SamBabu123456789 <sambabupolimetla@gmail.com>
2025-04-04 15:10:02 +0530

    done

---
+++ src/Destination.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Destination(props) {
+  return (
+    <div>
+      <h1>{props.jaffa.name}</h1>
+      <h1>{props.jaffa.location}</h1>
+      <img src={props.jaffa.img} alt="" />
+      <h1>{props.jaffa.description}</h1>
+      <h1>{props.jaffa.price}</h1>
+    </div>
+  )
+}
+
+export default Destination
